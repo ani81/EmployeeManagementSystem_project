a9d018cfe77cd9838d77c13a1aec5a014ecf13d3
ani81 <150571733+ani81@users.noreply.github.com>
2023-11-17 10:03:39 +0530

    Update EmployeeManagementSystemApplication.java

--- src/main/java/com/staff/EmployeeManagementSystemApplication.java
+++ src/main/java/com/staff/EmployeeManagementSystemApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeManagementSystemApplication {
 
+
+public static void mymethod(){
+	syso(This is my method );
+}
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
